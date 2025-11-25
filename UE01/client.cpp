0d92bbef383e066d021156faa2fedb23f95f77a0
dokoe92 <dokoe92@gmail.com>
2025-11-25 17:37:46 +0100

    added comments

--- UE01/client.cpp
+++ UE01/client.cpp
@@ -10,18 +10,20 @@
 #define MAX_LINE_LENGTH 100
 
 int main() {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    int sock = socket(AF_INET, SOCK_STREAM, 0); // Erzeugt TCP Socket
 
     if (sock < 0) {
         perror("socket");
         return 1;
     }
 
+    // Serveradresse vorbereiten
     sockaddr_in server;
     server.sin_family = AF_INET;
-    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
+    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr); // wandelt Adresse in 32 Bit Binärwert um
     server.sin_port = htons(8000);
 
+    // Mit Server verbinden
     if (connect(sock, reinterpret_cast<sockaddr *>(&server), sizeof(server)) < 0) {
         perror("connect failed");
         return 1;
@@ -32,14 +34,15 @@ int main() {
     char line[MAX_LINE_LENGTH];
     int ret_val = 0;
 
-    while ((ret_val = read(sock, line, MAX_LINE_LENGTH)) > 0) {
+    // 
+    while ((ret_val = read(sock, line, MAX_LINE_LENGTH)) > 0) { // Liest von Server
         line[ret_val] = '\0';
         std::cout << std::endl;
 
         std::string input;
         std::getline(std::cin, input);
 
-        write(sock, input.c_str(), input.length());
+        write(sock, input.c_str(), input.length()); // Sendet an Server
     }
 
     close(sock);
--- UE01/server.cpp
+++ UE01/server.cpp
@@ -6,7 +6,8 @@
 
 extern "C" void ServerProcess(int in, int ou);
 
-static int next_pending_connection(int server_sock) { // Funktion kann man von außen nciht referenzieren
+// Funktion holt nächste eingehende Verbinung - Neuer Client Socket
+static int next_pending_connection(int server_sock) { // Funktion kann man von außen nicht referenzieren
     int fd = accept(server_sock, nullptr, nullptr);
     std::cout << "\nAccepted connection" << std::endl;
     return fd;
@@ -14,6 +15,10 @@ static int next_pending_connection(int server_sock) { // Funktion kann man von a
 
 int main() {
     signal(SIGCHLD, SIG_IGN);
+
+    // ### #1 Socket Filedescriptor erzeugen
+
+    // Gibt Filedescriptor des Sockets zurück. AF(Adress Family -> IPv4), SOCK_STREAM(TCP), Protocol (0 ist Standard)
     int sock = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sock < 0) {
@@ -21,11 +26,17 @@ int main() {
         return 1;
     }
 
+    // ### #2 Socket an Adresse (+ Port) binden
+
+    // sockaddr ist Struktur für IPv4 Adresse
+    // htons braucht man wegen little / big endian
     sockaddr_in server{};
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(8000);
 
+    // Socket an eine Adresse + Port binden
+    // Listen (Server) Socket gebunden
     if (bind(sock, reinterpret_cast<sockaddr *>(&server), sizeof(server)) < 0) {
         perror("Bind failed");
         return 1;
@@ -33,6 +44,8 @@ int main() {
 
     std::cout << "Bound to port 8000" << std::endl;
 
+    // ### #3 Socket soll nun eingehende Verbindungen annehmen können. Hier max 10 gleichzeitige Verbindungen
+    // Server Socket lauscht nun
     if (listen(sock, 10) != 0 ) {
         perror("Listen failed");
         return 1;
@@ -40,26 +53,28 @@ int main() {
 
     std::cout << "\nWaiting for incoming connections..." << std::endl;
 
-    while (true) {
-        int fd = next_pending_connection(sock);
+    // ### #4 Verbindungen bearbeiten
+
+    while (true) { // Server wartet immer wieder auf neue Verbindungen
+        int fd = next_pending_connection(sock);  // File Descriptor der Client Verbindung
 
         if (fd < 0) {
             perror("Failed to get pending connection");
         } else {
 
-            int pid = fork();
+            int pid = fork(); // erzeugt neuen Prozess (Kopie des Elternprozess). Jeder Client beomm eigenen Prozess
 
             if (pid < 0) {
-                perror("Fork failed"); // noch im Elternprozess etwas schief gegangen
+                perror("Fork failed"); // noch im Elternprozess etwas schief gegangen. Weiter zur nächsten Verbindung
                 continue;
             }
 
-            if (pid == 0) { //Kindprozess gestartet
+            if (pid == 0) { //Kindprozess gestartet. Client wird behandelt. Kind wird terminiert
                 std::cout << "Child process started" << std::endl;
                 ServerProcess(fd, fd);
                 exit(0);
             }
-            close(fd);
+            close(fd); // Parent schließt nun Kindverbindung
         }
     }
 
